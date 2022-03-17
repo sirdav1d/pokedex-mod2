5cc47d4cc610864b7a30c2eab0633ce7f6058e84
sirdav1d <ddavid.diniz@gmail.com>
2022-03-17 08:32:55 -0300

    up

--- index.js
+++ index.js
@@ -2,32 +2,36 @@
 
 const express = require('express');
 const app = express();
+const path = require('path');
+
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const pokedex = [
 	{
 		id: 1,
-		nome: "Charmander",
-		description:"It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-		type: "Fire",
-		abilities: "Blaze",
-		imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png",
+		nome: 'Charmander',
+		description:
+			'It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.',
+		type: 'Fire',
+		abilities: 'Blaze',
+		imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png',
 	},
 	{
 		id: 2,
-		nome: "Vaporeon",
-		description:"When Vaporeon’s fins begin to vibrate, it is a sign that rain will come within a few hours.",
-		type: "Water",
-		abilities: "Water Absorb",
-		imagem: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png",
+		nome: 'Vaporeon',
+		description:
+			'When Vaporeon’s fins begin to vibrate, it is a sign that rain will come within a few hours.',
+		type: 'Water',
+		abilities: 'Water Absorb',
+		imagem: 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/134.png',
 	},
 ];
 
-app.set('view engine', 'ejs');
-
 app.get('/', (req, res) => {
-	res.render("index", {pokedex});
+	res.render('index', { pokedex });
 });
 
 app.listen(3000, () =>
-	console.log("Servidor rodando em http://localhost:3000"),
+	console.log('Servidor rodando em http://localhost:3000'),
 );
