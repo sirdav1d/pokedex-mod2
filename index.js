fb4e29f2c76e7422f1604c1ab85a012f0f3da5ae
sirdav1d <ddavid.diniz@gmail.com>
2022-03-19 20:33:56 -0300

    up

--- index.js
+++ index.js
@@ -59,6 +59,8 @@ app.post('/update/:id', (req, res) => {
 	res.redirect('/');
 });
 
-app.listen(3000, () =>
+const port = process.env.PORT || 3000;
+
+app.listen(port, () =>
 	console.log('Servidor rodando em http://localhost:3000'),
 );
